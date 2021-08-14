95e88c5f53ee1b03ff0b9913a28a73b3ecd0b661
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-08-14 13:15:52 -0400

    created React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './Trivia_Bot.png';
+
+function App() {
+  return (
+    <div >
+      <h1>Trivia Bot</h1>
+      <img src={logo} alt="Trivia Bot logo"/>
+    </div>
+  );
+}
+
+export default App;
